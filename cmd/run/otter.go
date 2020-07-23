ea14e15dba17afd8927164eb2f899b87bf10710f
Austin Ely <austiely@amazon.com>
2020-07-23 16:46:14 -0700

    add copilot dir and otter.go changes

--- cmd/run/otter.go
+++ cmd/run/otter.go
@@ -5,12 +5,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
-	"os"
-	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -43,39 +38,9 @@ func HealthCheck(w http.ResponseWriter, req *http.Request, ps httprouter.Params)
 	w.WriteHeader(http.StatusOK)
 }
 
-func writeS3() error {
-	sess := session.Must(session.NewSession())
-
-	uploader := s3manager.NewUploader(sess)
-
-	svcName := os.Getenv("COPILOT_SERVICE_NAME")
-	bucketname := os.Getenv("S3SSL_NAME")
-	now := time.Now().Unix()
-	fname := fmt.Sprintf("%s-test-startup-%d", svcName, now)
-
-	f, err := os.Create(fname)
-	if err != nil {
-		log.Printf("error creating file: %w\n", err)
-		return err
-	}
-
-	result, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(bucketname),
-		Key:    aws.String(fname),
-		Body:   f,
-	})
-	if err != nil {
-		log.Printf("error uploading: %w\n")
-		return err
-	}
-	log.Printf("success upload: %s", result.Location)
-	return nil
-}
-
 func main() {
-	writeS3()
 	r := httprouter.New()
 	r.GET("/healthcheck", HealthCheck)
-	r.GET("/", GetOtterStatus)
+	r.GET("/otter/status", GetOtterStatus)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
